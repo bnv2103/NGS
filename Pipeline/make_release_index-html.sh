dd68297c6e44ddd152eff068a99bd8c914290c43
Y. Joshua Shen <joshuashen@gmail.com>
2011-10-01 16:46:19 +0000

    make index.html file for web release

---
+++ Pipeline/make_release_index-html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -cwd
+
+
+rdir=$1
+
+cd $rdir
+
+flist=`ls`
+
+touch index.html
+echo -e "<html><body> <p><ul>" >> index.html
+
+
+for f in $flist;  do
+    s=`du -h $f | head -1 | cut -f1`
+    echo -e "<li><a href=$f>$f</a> &nbsp; &nbsp; ($s kb)</li>" >> index.html
+done
+
+echo -e "</ul></p></body></html>" >> index.html
