a2c7fb8ea5c245dd875c58515591106cda3f79c6
joshuas <joshuashen@gmail.com>
2012-02-16 11:55:16 -0500

    Update Exome/mapping-two-cores.sh

--- Exome/mapping-two-cores.sh
+++ Exome/mapping-two-cores.sh
@@ -90,7 +90,7 @@ if [[ ! $fastq2 == "" ]]; then  # paired-ends
     wait   ### need to wait the F reads finish
 
     date
-    cmd="$bwa sampe -r $rgheader  $REF $fastq1.sai $fastq2.sai $fastq1 $fastq2 | $samtools view -bS -  > $output.bam.temp"
+    cmd="$bwa sampe -P -r $rgheader  $REF $fastq1.sai $fastq2.sai $fastq1 $fastq2 | $samtools view -bS -  > $output.bam.temp"
     echo $cmd
     $bwa sampe -P -r $rgheader  $REF $fastq1.sai $fastq2.sai $fastq1 $fastq2 | $samtools view -bS -  > $output.bam.temp
 
@@ -104,9 +104,9 @@ else  # single-end
     wait
     
     date
-    cmd="$bwa samse -P -r $rgheader $REF $fastq1.sai $fastq1 | $samtools view -bS - >  $output.bam.temp"
+    cmd="$bwa samse  -r $rgheader $REF $fastq1.sai $fastq1 | $samtools view -bS - >  $output.bam.temp"
     echo $cmd
-    $bwa samse -P -r $rgheader $REF $fastq1.sai $fastq1 | $samtools view -bS - >  $output.bam.temp
+    $bwa samse  -r $rgheader $REF $fastq1.sai $fastq1 | $samtools view -bS - >  $output.bam.temp
 #    $samtools sort -m $sortmem  $output.bam.temp  $output
     date
     echo "bwa alignment complete. Sorting the bam file ..."
