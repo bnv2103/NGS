6e0da26f1d756c44450ea295b1ef3bd9ed974e7b
Samreen Zafer <samreen.jasvi@gmail.com>
2012-03-12 13:59:33 +0000

    used bc for floating point

--- utils/track_titan_space.sh
+++ utils/track_titan_space.sh
@@ -12,24 +12,31 @@ avail=`df -h /ifs/scratch/c2b2/ngs_lab/ngs/ |tail -1 | sed 's/ \+ /\t/g' |cut -f
 
 if [[ $avail =~ "T" ]];then
 	tera=`echo $avail | sed 's/T//'`
-	avail_byte=$(($tera*1024*1024*1024*1024))
+	avail_byte=`echo "$tera*1024*1024*1024*1024.0" | bc  -l | awk -F '.' '{ print $1; exit; }'`
 elif  [[ $avail =~ "G" ]];then
         tera=`echo $avail | sed 's/T//'`
-        avail_byte=$(($tera*1024*1024*1024))
+        avail_byte=`echo "$tera*1024*1024*1024.0" | bc  -l | awk -F '.' '{ print $1; exit; }'`
 elif  [[ $avail =~ "M" ]];then
         tera=`echo $avail | sed 's/T//'`
-        avail_byte=$(($tera*1024*1024))
+        avail_byte=`echo "$tera*1024*1024.0" | bc  -l | awk -F '.' '{ print $1; exit; }'`
 fi
 
-threshold=$((4*1024*1024*1024*1024))
+threshold=` echo "4*1024*1024*1024*1024.0" | bc  -l | awk -F '.' '{ print $1; exit; }'`
+echo $avail_byte
+echo $threshold
 
-if [[ $avail_byte -lt $threshold ]];
-then
-	echo "NGS Disk Space Alarm." > $dir/mailbody_$suff.txt	
-	echo "Available disk space : $avail " >> $dir/mailbody_$suff.txt
-        echo "Used disk space : $used " >> $dir/mailbody_$suff.txt
-        echo "Total disk space : $total " >> $dir/mailbody_$suff.txt
-	sh /ifs/data/c2b2/ngs_lab/ngs/code/NGS/Pipeline/sendMail.sh -s "NGS Disk Space Alarm" -t $to_email  -m $dir/mailbody_$suff.txt
-	rm $dir/mailbody_$suff.txt
+if [[ $avail_byte -lt $threshold ]];then
+	echo "getting less";
+else
+	echo "doing fine";
 fi
+
+#then
+#	echo "NGS Disk Space Alarm." > $dir/mailbody_$suff.txt	
+#	echo "Available disk space : $avail " >> $dir/mailbody_$suff.txt
+ #       echo "Used disk space : $used " >> $dir/mailbody_$suff.txt
+  #      echo "Total disk space : $total " >> $dir/mailbody_$suff.txt
+#	sh /ifs/data/c2b2/ngs_lab/ngs/code/NGS/Pipeline/sendMail.sh -s "NGS Disk Space Alarm" -t $to_email  -m $dir/mailbody_$suff.txt
+#	rm $dir/mailbody_$suff.txt
+#fi
 
