e385f93f4f64a39bb7d097a0ba26229bd2e78b78
Samreen Zafer <samreen.jasvi@gmail.com>
2012-04-13 17:39:27 +0000

    update qsub threads from 2 to 4 (if Paired end mapping)

--- Exome/do_mapping.sh
+++ Exome/do_mapping.sh
@@ -18,14 +18,15 @@ fi
 # example: RUNID_laneX_SAMPLEID_1.fastq
   job_id=`basename $fq |  sed 's/\_1.fastq//' `	#unique name for jobname (includes runid_lane#_sampleID
   g=`echo $fq | sed 's/_1.fastq/_3.fastq/'`
-  if  [ ! -e $g ];  then g="" ; fi
+  nthread=2
+  if  [ ! -e $g ];  then g="" ;  nthread=2 ;else   nthread=4;fi
   sampleName=`basename $fq | sed 's/\_1.fastq//' | cut -f6  -d '_'`	#acc to  new convention, to change to old do -f4
   
 if [[ $automated == "" ]]; #was NOT triggered by automatic pipeline
 then
-  cmd="qsub -pe smp 2 -R y -l mem=4G,time=48:: -o logs/mapping.$sampleName.o -e logs/mapping.$sampleName.e -N map.$job_id $BPATH/mapping-two-cores.sh -i $fq -p $g -z $sampleName -n $sampleName -s $setting -o mapping/$sampleName -t 2 -c 1 "
+  cmd="qsub -pe smp $nthread -R y -l mem=4G,time=48:: -o logs/mapping.$sampleName.o -e logs/mapping.$sampleName.e -N map.$job_id $BPATH/mapping-two-cores.sh -i $fq -p $g -z $sampleName -n $sampleName -s $setting -o mapping/$sampleName -t 2 -c 1 "
 else	#trigger automatic process
-  cmd="qsub -pe smp 2 -R y -l mem=4G,time=48:: -o logs/mapping.$sampleName.o -e logs/mapping.$sampleName.e -N map.$job_id.AUTO $BPATH/mapping-two-cores.sh -i $fq -p $g -z $sampleName -n $sampleName -s $setting -o mapping/$sampleName -t 2 -c 1 -A $automated"
+  cmd="qsub -pe smp $nthread -R y -l mem=4G,time=48:: -o logs/mapping.$sampleName.o -e logs/mapping.$sampleName.e -N map.$job_id.AUTO $BPATH/mapping-two-cores.sh -i $fq -p $g -z $sampleName -n $sampleName -s $setting -o mapping/$sampleName -t 2 -c 1 -A $automated"
 fi
   echo $cmd >> logs/history.$sampleName.txt
   $cmd
