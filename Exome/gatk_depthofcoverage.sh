1e795b8584c70da0bc2ff04ccf940df53c296bfe
Samreen Zafer <samreen.jasvi@gmail.com>
2012-05-02 16:48:10 +0000

    minor

--- Exome/gatk_depthofcoverage.sh
+++ Exome/gatk_depthofcoverage.sh
@@ -103,7 +103,7 @@ cat $INP.coverage.sample_summary  | grep -v total | grep -v Total >> $INP.reads.
 if [[ $AUTO != "" ]]
 then
         #Trigger automatic downstream steps : joint var calling
-	echo $INP > $INP.list
+	readlink -f $INP > $INP.list
 	path_inp=`dirname $INP`
 	cmd_varcalling="sh ${BPATH}/joint_SNV-indel_calling-split-by-intervals.sh -i $INP.list -m 6 -s $GLOBAL -n 2 -j 100 -d 300 -v 7 -t 30 -o $path_inp/VarCalling -A AUTO "
 	echo $cmd_varcalling
