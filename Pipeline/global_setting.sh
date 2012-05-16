6fb0083f0e12726e66260b6c1351db7f4d9deb4a
Nextgen Sequencing %Yufeng <ngs@b1a.c3.titan>
2012-05-16 14:28:01 +0000

    changed SampleSheets to point to SampleSheets2 (Oliviers copy)

--- Pipeline/global_setting.sh
+++ Pipeline/global_setting.sh
@@ -2,7 +2,7 @@ export HiSeqRuns=/ifs/scratch/c2b2/ngs_lab/ngs/Illumina/
 export FastqDir=/ifs/scratch/c2b2/ngs_lab/ngs/Fastq/
 export StatusDir=/ifs/data/c2b2/ngs_lab/ngs/status/
 export PIPEBASE=/ifs/data/c2b2/ngs_lab/ngs/code/NGS/Pipeline/
-export SampleSheets=/ifs/data/c2b2/ngs_lab/ngs/status/SampleSheets/
+export SampleSheets=/ifs/data/c2b2/ngs_lab/ngs/status/SampleSheets2/
 export QueueLog=/ifs/data/c2b2/ngs_lab/ngs/status/QueueLog/
 export RUBY18="/ifs/data/c2b2/ngs_lab/ngs/usr/local/bin/ruby"
 
