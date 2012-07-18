269e4b9818359b80eca4ca86dcd9b28e851eeb33
Samreen Zafer <samreen.jasvi@gmail.com>
2012-07-18 19:05:56 +0000

    scripts for QC plots from Fastq reads

---
+++ utils/QCplot.R
@@ -0,0 +1,56 @@
+args <- commandArgs(TRUE)
+
+numReads = as.numeric(args[1])
+outfile = args[2]
+actgfile = args[3]
+qualfile = args[4]
+qscore_f = args[5]
+qscore_r = args[6]
+
+a = read.table(file=actgfile, header=F)
+readLength = length(a[1,])-1
+
+b = a[,2:(readLength+1)]
+A = b[1,]
+T = b[2,]
+C = b[3,]
+G = b[4,]
+
+A = A / numReads
+T = T / numReads
+C = C / numReads
+G = G / numReads
+
+x = 1:readLength
+pdf(file = outfile)
+
+plot(0, xlim=c(1,readLength), ylim=c(0,1), xlab = "Cycle", ylab = "Frequency", main="ACGT Distribution",  type='n')
+lines(x, A, lty=1, col="blue")
+lines(x, C, lty=1, col="green")
+lines(x, T, lty=1, col="red")
+lines(x, G, lty=1, col="black")
+legend(80, 0.8, lty=c(1,1,1,1), col=c("blue", "green", "red", "black"),legend=c("A", "C", "T", "G"))
+
+qual1 = read.table(file=qualfile, header=F)
+qual1 = as.numeric(qual1)
+qual1 = qual1/numReads
+plot(0, xlim=c(1,readLength), ylim=c(0,80), xlab = "Cycle", ylab = "Phred Quality", main="Mean Quality by Cycle", type='n')
+lines(x, qual1, lty =1, col="darkgreen")
+
+
+qscore1 = read.table(file=qscore_f, header=F)
+maxX = nrow(qscore1)
+qscore1 = as.numeric(t(qscore1))
+temp= 1:maxX
+maxY = max(qscore1)
+plot(0, xlim=c(1,45), ylim=c(0,maxY),  xlab = "QScore", ylab = "Frequency", main="QScore Histogram Read1",  type='n')
+lines(temp, qscore1, lty=1, col="blue")
+
+qscore2 = read.table(file=qscore_r, header=F)
+qscore2 = as.numeric(t(qscore2))
+maxY = max(qscore2)
+plot(0, xlim=c(1,45), ylim=c(0,maxY),  xlab = "QScore", ylab = "Frequency", main="QScore Histogram Read3",  type='n')
+lines(temp, qscore2, lty=1, col="green")
+
+dev.off()
+
---
+++ utils/fastq_QCplot.rb
@@ -0,0 +1,185 @@
+require 'csv'
+
+$VERBOSE = nil
+
+## Plots the Quality Distribution, ACTG per cycle from raw fastq reads. Can take paired end file as 3rd argument. 
+def main
+  output = ARGV[0] #pdf file
+  fq = ARGV[1] # sampl.fastq
+  fq3 = ARGV[2] # sampl.fastq3 if there is 
+
+  qground = 64 ## For illumina 1.3+, 1.5+ use -64, for illumina 1.8+ use -33 
+
+  outfile = File.new("#{output}_ACTG.txt", "w")
+  outfile1 = File.new("#{output}_Qual.txt", "w")
+  qsc_f = File.new("#{output}_QScore_f.txt", "w")
+  qsc_r = File.new("#{output}_QScore_r.txt", "w")
+  qsc_f_hist = File.new("#{output}_QScore_f_hist.txt", "w")
+  qsc_r_hist = File.new("#{output}_QScore_r_hist.txt", "w")
+  utils = "/ifs/data/c2b2/ngs_lab/ngs/code/NGS/utils/"
+
+  numReads = 0
+  slen=0
+  if fq3.nil? 
+	pair = 1
+  else
+	if File.exists?(fq3)
+		pair =2
+	else
+		pair =1
+	end
+  end
+ 
+  qscore_bucket_fwd = Array.new(45){ |i| 0*i }
+  qscore_bucket_rvs = Array.new(45){ |i| 0*i }
+  infile = File.new(fq, "r")
+
+while !infile.eof?
+    s = infile.take(1)
+    readName = s
+    s = infile.take(1)
+    s = s.to_s
+    s = s[2..-5]
+    readSeq = s
+
+   if numReads ==0
+     slen = s.size
+     qual = Array.new(slen*pair){ |i| 0*i }
+     valueA = Array.new(slen*pair) { |i| 0*i }
+     valueT = Array.new(slen*pair) { |i| 0*i }
+     valueC = Array.new(slen*pair) { |i| 0*i }
+     valueG = Array.new(slen*pair) { |i| 0*i }
+   end
+
+    s = infile.take(1)
+    s = infile.take(1)
+    s = s.to_s
+    s = s[2..-5]
+    readQual = s
+    numReads = numReads + 1
+    for i in 0..(slen-1)		##Extract ACTG info
+      bp = readSeq[i]
+      if bp != nil
+        if bp == "A"
+          valueA[i] = valueA[i] + 1
+        elsif bp =="T"
+          valueT[i] = valueT[i] + 1
+        elsif bp =="C"
+          valueC[i] = valueC[i] + 1
+        elsif bp =="G"
+          valueG[i] = valueG[i] + 1
+        end
+      end
+    end 
+    for i in 0..(slen-1)		##Extract Quality distribution info
+      bp = readQual[i]
+      if bp != nil
+        q = readQual[i].ord - qground
+        qual[i] = qual[i] + q        
+      end
+    end 
+    aqscore=0   		#Extract Qscore histogram info
+    readQual.each_char do | item |
+	aqscore += (item.ord - qground)
+    end
+        avgqscore = aqscore/slen
+        qsc_f.puts avgqscore    #Extract Qscore histogram info
+        qscore_bucket_fwd[avgqscore]+=1
+  end
+
+if pair == 2
+infile.close
+infile = File.new(fq3, "r")
+  while !infile.eof?
+    s = infile.take(1)
+    readName = s
+    s = infile.take(1)
+    s = s.to_s
+    s = s[2..-5]
+    readSeq = s
+    s = infile.take(1)
+    s = infile.take(1)
+    s = s.to_s
+    s = s[2..-5]
+    readQual = s
+    numReads = numReads + 1
+    for j in 0..(slen -1)	##Extract ACTG info
+      bp = readSeq[j]
+	i = (j + slen)
+      if bp != nil
+        if bp == "A"
+          valueA[i] = valueA[i] + 1
+        elsif bp =="T"
+          valueT[i] = valueT[i] + 1
+        elsif bp =="C"
+          valueC[i] = valueC[i] + 1
+        elsif bp =="G"
+          valueG[i] = valueG[i] + 1
+        end
+      end
+    end 
+    for j in 0..(slen-1)		##Extract Quality distribution info
+      bp = readQual[j]
+        i = (j + slen)
+      if bp != nil
+        q = readQual[j].ord - qground
+        qual[i] = qual[i] + q         
+      end
+    end    
+    aqscore=0
+    readQual.each_char do | item |
+        aqscore += (item.ord - qground)
+    end
+        avgqscore = aqscore/slen
+	qsc_r.puts avgqscore	#Extract Qscore histogram info
+	qscore_bucket_rvs[avgqscore]+=1
+  end
+end
+
+  numReads = numReads -1
+  outfile.print "A \t"
+  for i in 0..(slen*pair)
+    outfile.print "#{valueA[i]} \t"
+  end
+  outfile.print "\n T \t"
+  for i in 0..(slen*pair)
+    outfile.print "#{valueT[i]} \t"
+  end
+  outfile.print "\n C \t"
+  for i in 0..(slen*pair)
+    outfile.print "#{valueC[i]} \t"
+  end
+  outfile.print "\n G \t"
+  for i in 0..(slen*pair)
+    outfile.print "#{valueG[i]} \t"
+  end
+  outfile.print "\n"
+  # outfile.print "\n qual \t"
+  for i in 0..(slen*pair)
+     outfile1.print "#{qual[i]} \t"
+  end
+
+  qscore_bucket_fwd.each do | item |
+	qsc_f_hist.puts item
+  end
+  qscore_bucket_rvs.each do | item |
+        qsc_r_hist.puts item
+  end
+
+  outfile1.print "\n"
+  outfile1.close
+  infile.close
+  outfile.close
+  qsc_r_hist.close
+  qsc_f_hist.close
+  qsc_r.close
+  qsc_f.close
+
+
+puts "Rscript #{utils}/QCplot_qscore.R  #{numReads} #{output}.pdf #{output}_ACTG.txt #{output}_Qual.txt #{output}_QScore_f_hist.txt #{output}_QScore_r_hist.txt "
+
+`Rscript #{utils}/QCplot_qscore.R #{numReads} #{output}.pdf   #{output}_ACTG.txt #{output}_Qual.txt #{output}_QScore_f_hist.txt #{output}_QScore_r_hist.txt `
+
+end
+
+main()
