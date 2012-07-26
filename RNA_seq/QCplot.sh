49efcae51f6296426530d01548daaf1dede008da
Xiaoyun Sun <xs2182@c2b2.columbia.edu>
2012-07-26 15:15:03 +0000

    XS modified

--- RNA_seq/QCplot.sh
+++ RNA_seq/QCplot.sh
@@ -4,7 +4,7 @@
 infileDir=$1 # input fastq file dir : infileDir for sample.bam
 # outfile=$2 # output pdf file : infileDir/sample.pdf
 
-# cd $infile
+# cd $infileDir
 ruby /ifs/scratch/c2b2/ngs_lab/xs2182/code/QCplot.rb $infileDir 
 
 
