11e645b42bc93129beda00454b6de9c446a626b0
Samreen Zafer <samreen.jasvi@gmail.com>
2012-07-31 20:51:18 +0000

    minor

--- Exome/make_release_summary.sh
+++ Exome/make_release_summary.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #$ -cwd
 
+#e.g combined.complete.annotated.vcf
+
 if [[ $1 == "" ]];then
-	echo "Missing argument Directory"
+	echo "Missing argument raw VCF file"
 	exit
 else
-	DIR=$1
+	vcf=`readlink -f $1`
+	DIR=`dirname $vcf`
 fi
 
 EXOMEBASE="/ifs/data/c2b2/ngs_lab/ngs/code/NGS/Exome/"
 UTILS="/ifs/data/c2b2/ngs_lab/ngs/code/NGS/utils/"
-
+WebRelease="/ifs/data/shares/deepsequencing/solid/public_html/"
+PIPEBASE="/ifs/data/c2b2/ngs_lab/ngs/code/NGS/Pipeline/"
 
 # summary stats on depth: 
 ## reads mapped to whole genome
@@ -21,30 +25,23 @@ if [ -e $DIR/release ];then
 else
 	release="$DIR/release"
 fi
-mkdir $release
+ mkdir $release
 cd $release
 
-#SNS release (PASS filter)
-sh $EXOMEBASE/do_release.sh $DIR/VarCalling/list.vcf-files.txt.vcf.complete.annotated.vcf.filtered.vcf.snv
-# indel release:
-sh $EXOMEBASE/do_release.sh $DIR/VarCalling/list.vcf-files.txt.vcf.complete.annotated.vcf.indel.filtered.vcf
-ln -s $DIR/VarCalling/list.vcf-files.txt.vcf.complete.annotated.vcf raw.variants.vcf
-ln -s $DIR/VarCalling/list.vcf-files.txt.vcf.complete.annotated.vcf.filtered.vcf.snv.release SNVs.vcf
-ln -s $DIR/VarCalling/list.vcf-files.txt.vcf.complete.annotated.vcf.indel.filtered.vcf.release indels.vcf 
-
-# Polyphen2 and SIFT scores. (use annovar)
-qsub -o $DIR/logs/annovar.o -e $DIR/logs/annovar.e $EXOMEBASE/do_annovar.sh $release/SNV.vcf
-
-perl $UTILS/convert_vcf_exomeannotation.pl  SNVs.vcf 
-perl $UTILS/convert_vcf_exomeannotation.pl  indels.vcf indel
+ ln -s $vcf raw.variants.vcf
+ ln -s $vcf.filtered.vcf.snv.release SNVs.vcf
+ ln -s $vcf.indel.filtered.vcf.release indels.vcf 
 
 ## SNV summary stats:
 # coding regions
-ruby $UTILS/vcf_transition-transversion-per-sample.rb  SNVs.vcf > summary.coding.xls 
+ ruby $UTILS/vcf_transition-transversion-per-sample.rb  SNVs.vcf > summary.coding.xls 
 # non-coding regions
-ruby $UTILS/vcf_transition-transversion-per-sample.rb  SNVs.vcf -1 > summary.noncoding.xls 
+ ruby $UTILS/vcf_transition-transversion-per-sample.rb  SNVs.vcf -1 > summary.noncoding.xls 
 
-for f in `ls $DIR/mapping/*refine/all.recalibrated.bam`; do echo $f; done > list.recalib_bam.list.release
+for f in `cat $DIR/vcf.list ` ; do
+	g=`echo $f | sed 's/_refine\/.\+/_refine\/all.recalibrated.bam/'` ; 
+	echo `readlink -f $g `;
+done > list.recalib_bam.list.release
 
 echo "" >summary.a
 echo "" >summary.b
@@ -53,7 +50,8 @@ echo "" >summary.d
 
 for f in `cat list.recalib_bam.list.release`; do 
 	head -2 $f.coverage.sample_summary | tail -1 |cut -f1 >>summary.a
-	mappedreads=`grep total $f.reads.mapped | sed 's/ \+/\t/'|cut -f1`
+        g=`echo $f | sed 's/_refine\/all.recalibrated.bam/.flagstat/'` ;
+	mappedreads=`grep total $g | sed 's/ \+/\t/'|cut -f1`
 	echo $mappedreads >>summary.b
 	head -2 $f.coverage.sample_summary | tail -1 |cut -f2,3,5,7,8,9,10,11  >>summary.c
 	targetreads=`head -2 $f.coverage.sample_summary | tail -1 |cut -f2 ` 
@@ -70,12 +68,12 @@ rm summary.b
 rm summary.c
 rm summary.d
 
+#E.g. /ifs/scratch/c2b2/ngs_lab/ngs/Projects/Exome-seq/120611_ALI_YIFU_20_HUMAN_EXOME_60X_PE_HISEQ/120629_SN828_0140_AD13G3ACXX/mapping_redo/Pulse-2.bam_refine/VarCalling/list.vcf-files.txt.vcf
 
-tem=`dirname $DIR`
-project=`basename $tem`
+project=` echo $DIR | tr "/" "\t" | cut -f9  `
 samp=`wc -l list.recalib_bam.list.release | awk '{print $1;}'`
 
-echo -e "This data is released at http://genomecenter.columbia.edu/ngs/  \n" > README
+echo -e "This data is released at http://genomecenter.columbia.edu/ngs/$project  \n" > README
 echo -e "Date : `date +%x` \n" >> README
 echo -e "ProjectID : $project \n" >> README
 echo -e "Note: \n" >> README
@@ -93,4 +91,14 @@ echo -e "  Data on the web will be available for a month, after that, the data m
 echo -e "  Pease write to sequencing@columbia.edu for questions. \n" >> README
 
 rm  list.recalib_bam.list.release
+
+if [ ! -d $WebRelease/$project ]
+then
+	mkdir $WebRelease/$project 
+	cp * $WebRelease/$project/
+	sh $PIPEBASE/make_release_index-html.sh $WebRelease/$project/
+	echo "Web Release Created!"
+else
+        echo "Web Release NOT created!"
+fi
 
